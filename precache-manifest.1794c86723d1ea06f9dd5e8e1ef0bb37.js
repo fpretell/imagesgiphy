2a5d7844fe61962762e9630e4717a2fb3f4ea902
Felix Pretell <felix@ipsumapp.co>
2020-06-28 02:51:17 -0400

    commit inicial

---
+++ precache-manifest.1794c86723d1ea06f9dd5e8e1ef0bb37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "462fe00cee7a726b971acb84d98f9050",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e10bf3b8bfea8b8f6aac",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e10bf3b8bfea8b8f6aac",
+    "url": "/static/js/main.4a47645e.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
